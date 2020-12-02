95dba91639180187e6c7b1f0d4803b4bfbab5c08
Alex M <alex@matros.ca>
2020-12-01 21:51:05 -0500

    day 1

---
+++ day1/solutions/Part1.java
@@ -0,0 +1,38 @@
+package solutions;
+
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
+
+public class Part1 {
+    public static List<String> readFileInList(String fileName)
+    {
+
+        List<String> lines = Collections.emptyList();
+        try {
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void main(String[] args)
+    {
+        List<String> inputData = readFileInList("day1\\input\\input.txt");
+
+        for (int i = 0; i < inputData.size(); i++) {
+            for (int j = i; j < inputData.size(); j++) {
+                int firstNum = Integer.parseInt(inputData.get(i));
+                int secondNum = Integer.parseInt(inputData.get(j));
+                if (firstNum + secondNum == 2020) {
+                    System.out.println(firstNum * secondNum);
+                }
+            }
+        }
+    }
+} 
---
+++ day1/solutions/Part2.java
@@ -0,0 +1,41 @@
+package solutions;
+
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
+
+public class Part2 {
+    public static List<String> readFileInList(String fileName)
+    {
+
+        List<String> lines = Collections.emptyList();
+        try {
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void main(String[] args)
+    {
+        List<String> inputData = readFileInList("day1\\input\\input.txt");
+
+        for (int i = 0; i < inputData.size(); i++) {
+            for (int j = i; j < inputData.size(); j++) {
+                for (int k = j; k < inputData.size(); k++) {
+                    int firstNum = Integer.parseInt(inputData.get(i));
+                    int secondNum = Integer.parseInt(inputData.get(j));
+                    int thirdNum = Integer.parseInt(inputData.get(k));
+                    if (firstNum + secondNum + thirdNum == 2020) {
+                        System.out.println(firstNum * secondNum * thirdNum);
+                    }
+                }
+            }
+        }
+    }
+}
