1401da2cdcd38074dbb9eef6aef7a857a51c6fd1
Alex M <alex@matros.ca>
2020-12-01 22:11:12 -0500

    day 1

--- src/day1/solutions/Part1.java
+++ src/day1/solutions/Part1.java
@@ -1,11 +1,11 @@
-package solutions;
+package day1.solutions;
 
 import java.util.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.io.*;
 
-public class Part2 {
+public class Part1 {
     public static List<String> readFileInList(String fileName)
     {
 
@@ -23,19 +23,16 @@ public class Part2 {
 
     public static void main(String[] args)
     {
-        List<String> inputData = readFileInList("day1\\input\\input.txt");
+        List<String> inputData = readFileInList("src\\day1\\input\\input.txt");
 
         for (int i = 0; i < inputData.size(); i++) {
             for (int j = i; j < inputData.size(); j++) {
-                for (int k = j; k < inputData.size(); k++) {
-                    int firstNum = Integer.parseInt(inputData.get(i));
-                    int secondNum = Integer.parseInt(inputData.get(j));
-                    int thirdNum = Integer.parseInt(inputData.get(k));
-                    if (firstNum + secondNum + thirdNum == 2020) {
-                        System.out.println(firstNum * secondNum * thirdNum);
-                    }
+                int firstNum = Integer.parseInt(inputData.get(i));
+                int secondNum = Integer.parseInt(inputData.get(j));
+                if (firstNum + secondNum == 2020) {
+                    System.out.println(firstNum * secondNum);
                 }
             }
         }
     }
-}
+} 
